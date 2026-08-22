a0ff82741e6aaf9f6f0dae056e91401bb3f04025
CallMeDirect <callmedirect0@gmail.com>
2026-08-23 01:08:34 +0700

    Added fake monitoring serivce for tests

---
+++ DevPulse.Tests/FakeSystemMonitorService.cs
@@ -0,0 +1,15 @@
+﻿using DevPulse.Models;
+using DevPulse.Services;
+
+namespace DevPulse.Tests;
+
+internal sealed class FakeSystemMonitorService(SystemStats stats) : ISystemMonitorService
+{
+    public Task<SystemStats> GetStatsAsync(
+        CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        return Task.FromResult(stats);
+    }
+}
